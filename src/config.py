7681fee6807ae812f19dbe84b723d1278b67d6f6
Dharmik Anghan <dharmikanghan09@gmail.com>
2024-03-20 18:36:51 +0530

    [Dharmik] Config and requirements.txt

---
+++ src/config.py
@@ -0,0 +1,14 @@
+import os 
+
+class Config:
+    SECRET_KEY=os.environ["SECRET_KEY"]
+    DB_NAME=os.environ["DB_NAME"]
+    DB_USER=os.environ["DB_USER"]
+    DB_PASSWORD=os.environ["DB_PASSWORD"]
+    DB_HOST=os.environ["DB_HOST"]
+    DB_PORT=os.environ["DB_PORT"]
+    EMAIL_USER=os.environ["EMAIL_USER"]
+    EMAIL_PASS=os.environ["EMAIL_PASS"]
+    EMAIL_FROM=os.environ["EMAIL_FROM"]
+    JWT_ACCESS_TOKEN_LIFETIME=os.environ["JWT_ACCESS_TOKEN_LIFETIME"]
+    JWT_REFRESH_TOKEN_LIFETIME=os.environ["JWT_REFRESH_TOKEN_LIFETIME"]
